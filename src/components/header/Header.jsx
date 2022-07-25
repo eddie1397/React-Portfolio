7cf1ddc58ed2a66617a86ddf28e82ffa2d4975bf
Eddie Binion <eddiebinion2@gmail.com>
2022-07-25 15:01:47 -0500

    Added CTA to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 
 // Start HERE
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header_container">
+        <h5> Hello I'm </h5>
+        <h1>Edward Binion Jr.</h1>
+        <h5 className="text-light">Software Engineer and Photographer</h5>
+        <CTA />
+
+      </div>
+    </header>
   )
 }
 
