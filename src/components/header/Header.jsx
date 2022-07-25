9bed37f9064d5d5fb501257735282c53864aa23c
Eddie Binion <eddiebinion2@gmail.com>
2022-07-25 15:22:58 -0500

    starting header.css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 
 // Start HERE
@@ -13,7 +15,13 @@ function Header() {
         <h1>Edward Binion Jr.</h1>
         <h5 className="text-light">Software Engineer and Photographer</h5>
         <CTA />
+        <HeaderSocials />
 
+        <div className="me">
+          <img src={ME} alt="Me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </header>
   )
