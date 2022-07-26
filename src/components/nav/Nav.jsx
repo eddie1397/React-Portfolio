0c4cf6e3fcd4c679d1b6ee045f32f2f48eeb22f7
Eddie Binion <eddiebinion2@gmail.com>
2022-07-26 10:13:21 -0500

    nav Icons added

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {BsCamera2} from 'react-icons/bs'
+import {BsCodeSlash} from 'react-icons/bs'
+import {MdOutlineDesignServices} from 'react-icons/md'
+import {RiServiceLine} from 'react-icons/ri'
+import {TbMessageCircle} from 'react-icons/tb'
+
+
 
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><AiOutlineHome /></a>
+      <a href="#about"><AiOutlineUser /></a>
+      <a href="#experience"><BiBook /></a>
+      <a href="#services"><RiServiceLine /></a>
+      <a href="#portfolio"><BsCodeSlash /></a>
+      <a href="#concept"><MdOutlineDesignServices /></a>
+      <a href="#contact"><TbMessageCircle /></a>
+
+    </nav>
   )
 }
 
