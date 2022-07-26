0c7397463385d3387e42cdce2c0aeba4e7357ed7
Eddie Binion <eddiebinion2@gmail.com>
2022-07-26 13:32:48 -0500

    finished JSX for Experience.jsx

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,114 @@
 import React from 'react'
 import './experience.css'
+import {BsFillPatchCheckFill} from 'react-icons/bs'
 
 function Experience() {
   return (
-    <section id="experience"> Experience </section>
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience-content">
+            <article className='experience__details'>
+              <BsFillPatchCheckFill />
+              <h4>HTML</h4>
+              <small class='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience__details'>
+              <BsFillPatchCheckFill />
+              <h4>CSS</h4>
+              <small class='text-light'>Intermediate</small>
+            </article>
+
+            <article className='experience__details'>
+              <BsFillPatchCheckFill />
+              <h4>Javascript</h4>
+              <small class='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience__details'>
+              <BsFillPatchCheckFill />
+              <h4>Material Design</h4>
+              <small class='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience__details'>
+              <BsFillPatchCheckFill />
+              <h4>Bootstrap</h4>
+              <small class='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience__details'>
+              <BsFillPatchCheckFill />
+              <h4>Tailwind</h4>
+              <small class='text-light'>Experienced</small>
+            </article>
+
+            <article className='experience__details'>
+              <BsFillPatchCheckFill />
+              <h4>React</h4>
+              <small class='text-light'>Experienced</small>
+            </article>
+          </div>
+        </div>
+
+
+      {/* END OF FRONT END */}
+        
+        
+        <div className="experience-backend">
+          <h3>Backend Development</h3>
+            <div className="experience-content">
+              <article className='experience__details'>
+                <BsFillPatchCheckFill />
+                <h4>Node.js</h4>
+                <small class='text-light'>Experienced</small>
+              </article>
+
+              <article className='experience__details'>
+                <BsFillPatchCheckFill />
+                <h4>MongoDB</h4>
+                <small class='text-light'>Intermediate</small>
+              </article>
+
+              <article className='experience__details'>
+                <BsFillPatchCheckFill />
+                <h4>Javascript</h4>
+                <small class='text-light'>Experienced</small>
+              </article>
+
+              <article className='experience__details'>
+                <BsFillPatchCheckFill />
+                <h4>Python</h4>
+                <small class='text-light'>Experienced</small>
+              </article>
+
+              <article className='experience__details'>
+                <BsFillPatchCheckFill />
+                <h4>PHP</h4>
+                <small class='text-light'>Experienced</small>
+              </article>
+
+              <article className='experience__details'>
+                <BsFillPatchCheckFill />
+                <h4>MySQL</h4>
+                <small class='text-light'>Experienced</small>
+              </article>
+
+              <article className='experience__details'>
+                <BsFillPatchCheckFill />
+                <h4>React</h4>
+                <small class='text-light'>Experienced</small>
+              </article>
+            </div>
+          </div>
+      </div>
+    </section>
   )
 }
-
+ 
 export default Experience
