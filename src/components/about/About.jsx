df435d1383ea6d36cbfbed9de50114aa7b1bcc86
Eddie Binion <eddiebinion2@gmail.com>
2022-07-26 13:16:16 -0500

    About Me Corrected

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -13,44 +13,51 @@ const about = () => {
 
       <div className="container about__container">
         <div className="about__me">
-          <img src={ME} alt="About Me Image" />
+          <div className="about__me-image">
+            <img src={ME} alt="About Me Image" />
+          </div>
         </div>
+
         {/* BOX 1 */}
         <div className="about__content">
           <div className="about__cards">
             <article className="about__card">
               <FaAward className="about__icon"/>
               <h5>Experience</h5>
-              <small>3+ Years Working Experience</small>
+              <small>3+ Years Working </small>
             </article>
-          </div>
-        </div>
+        
         {/* BOX 2 */}
-        <div className="about__content">
-          <div className="about__cards">
-            <article className="about__card">
-              <FiUsers className="about__icon"/>
-              <h5>Clients</h5>
-              <small>200+ Clients Worldwide</small>
-            </article>
+          <div className="about__content">
+              <article className="about__card">
+                <FiUsers className="about__icon"/>
+                <h5>Clients</h5>
+                <small>200+ Clients Worldwide</small>
+              </article>
+          </div>
+
+          {/* BOX 3 */}
+          <div className="about__content">
+              <article className="about__card">
+                <VscFolderLibrary className="about__icon"/>
+                <h5>Projects </h5>
+                <small>80+ Projects Completed</small>
+              </article>
+            </div>
+          {/* BOX 4 */}
+            <div className="about__content">
+              <article className="about__card">
+                <FiUsers className="about__icon"/>
+                <h5>Example</h5>
+                <small>Can Be Easily Removed!</small>
+              </article>
           </div>
         </div>
-        {/* BOX 3 */}
-        <div className="about__content">
-          <div className="about__cards">
-            <article className="about__card">
-              <VscFolderLibrary className="about__icon"/>
-              <h5>Projects </h5>
-              <small>80+ Projects Completed</small>
-            </article>
-          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima illo praesentium consequatur ad facere, adipisci placeat. Voluptatum, esse magnam beatae vero ut natus ipsum voluptatem, consequatur ad eveniet fugiat aliquam.
+          </p>
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
         </div>
-
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima illo praesentium consequatur ad facere, adipisci placeat. Voluptatum, esse magnam beatae vero ut natus ipsum voluptatem, consequatur ad eveniet fugiat aliquam.
-        </p>
-
-        <a href="#contact" className="btn btn-primary">Let's Talk</a>
       </div>
     </section>
   )
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -20,9 +20,9 @@ const Nav = () => {
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav==='#' ? 'active' : ''}><AiOutlineHome /></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav==='#about' ? 'active': ''}><AiOutlineUser /></a>
       <a href="#experience" onClick={() =>setActiveNav('#experience')} className={activeNav==='#experience' ? 'active': ''}><BiBook /></a>
-      <a href="#services" onClick={() =>setActiveNav('#services')} className={activeNav==='#services' ? 'active': ''}><RiServiceLine /></a>
+      {/* <a href="#services" onClick={() =>setActiveNav('#services')} className={activeNav==='#services' ? 'active': ''}><RiServiceLine /></a> */}
       <a href="#portfolio" onClick={() =>setActiveNav('#portfolio')} className={activeNav==='#portfolio' ? 'active': ''}><BsCodeSlash /></a>
-      <a href="#concept" onClick={() =>setActiveNav('#concept')} className={activeNav==='#concept' ? 'active': ''}><MdOutlineDesignServices /></a>
+      {/* <a href="#concept" onClick={() =>setActiveNav('#concept')} className={activeNav==='#concept' ? 'active': ''}><MdOutlineDesignServices /></a> */}
       <a href="#contact" onClick={() =>setActiveNav('#contact')} className={activeNav==='#contact' ? 'active': ''}><TbMessageCircle /></a>
 
     </nav>
