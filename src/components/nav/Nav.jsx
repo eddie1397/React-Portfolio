0b0b5cc67f9c12733c84691f48cdd99b263b40b2
Eddie Binion <eddiebinion2@gmail.com>
2022-07-26 11:04:40 -0500

    updated Nav.jsx with active styles

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -8,20 +8,22 @@ import {BsCodeSlash} from 'react-icons/bs'
 import {MdOutlineDesignServices} from 'react-icons/md'
 import {RiServiceLine} from 'react-icons/ri'
 import {TbMessageCircle} from 'react-icons/tb'
+import {useState} from 'react'
 
 
 
 
-function Nav() {
+const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#');
   return (
     <nav>
-      <a href="#"><AiOutlineHome /></a>
-      <a href="#about"><AiOutlineUser /></a>
-      <a href="#experience"><BiBook /></a>
-      <a href="#services"><RiServiceLine /></a>
-      <a href="#portfolio"><BsCodeSlash /></a>
-      <a href="#concept"><MdOutlineDesignServices /></a>
-      <a href="#contact"><TbMessageCircle /></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav==='#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav==='#about' ? 'active': ''}><AiOutlineUser /></a>
+      <a href="#experience" onClick={() =>setActiveNav('#experience')} className={activeNav==='#experience' ? 'active': ''}><BiBook /></a>
+      <a href="#services" onClick={() =>setActiveNav('#services')} className={activeNav==='#services' ? 'active': ''}><RiServiceLine /></a>
+      <a href="#portfolio" onClick={() =>setActiveNav('#portfolio')} className={activeNav==='#portfolio' ? 'active': ''}><BsCodeSlash /></a>
+      <a href="#concept" onClick={() =>setActiveNav('#concept')} className={activeNav==='#concept' ? 'active': ''}><MdOutlineDesignServices /></a>
+      <a href="#contact" onClick={() =>setActiveNav('#contact')} className={activeNav==='#contact' ? 'active': ''}><TbMessageCircle /></a>
 
     </nav>
   )
