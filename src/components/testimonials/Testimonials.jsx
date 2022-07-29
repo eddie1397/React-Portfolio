f7b0bd1351e40f912fd52457a00fcd3c2466249a
Eddie Binion <eddiebinion2@gmail.com>
2022-07-29 09:47:40 -0500

    added images for avatars

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
 
 function Testimonials() {
   return (
-    <section id="testimonials">Testimonials</section>
+    <section id="testimonials">
+      <h5>Review from Clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article className='testimonial'>
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar Image" />
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
