065629f2cbca2c203c9eb43fe9152e35ca844e17
Eddie Binion <eddiebinion2@gmail.com>
2022-07-29 11:47:44 -0500

    changes made

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import {AiOutlineWhatsApp} from 'react-icons/ai'
 
 const Contact = () => {
   return (
@@ -10,12 +13,34 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className='contact__option'>
-            
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>eddiebinion2@gmail.com</h5>
+            <a href="mailto: eddiebinion2@gmail.com" target='_blank'>Send a message</a>
+          </article>
+
+          <article className='contact__option'>
+            <RiMessengerLine className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>Edward Binion</h5>
+            <a href="https://m.me/eddie.binion" target='_blank'>Send a message</a>
+          </article>
+
+          <article className='contact__option'>
+            <AiOutlineWhatsApp className='contact__option-icon'/>
+            <h4>WhatsApp</h4>
+            <h5>+123456789</h5>
+            <a href="https://apiwhatsapp.com/send?phone=+7737592508" target='_blank'>Send a message</a>
           </article>
         </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form action="">
+          <input type="text"name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
       </div>
-      {/* END OF CONTACT OPTIONS */}
-      <form action=""></form>
     </section>
   )
 }
