f09c9f7de9dbd047aa0ef84d31c70d782c46671f
Eddie Binion <eddiebinion2@gmail.com>
2022-09-28 09:02:30 -0500

    About me completed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -24,7 +24,7 @@ const about = () => {
             <article className="about__card">
               <FaAward className="about__icon"/>
               <h5>Experience</h5>
-              <small>3+ Years Working </small>
+              <small>3+ Years Working</small>
             </article>
         
         {/* BOX 2 */}
@@ -32,7 +32,7 @@ const about = () => {
               <article className="about__card">
                 <FiUsers className="about__icon"/>
                 <h5>Clients</h5>
-                <small>200+ Clients Worldwide</small>
+                <small>12+ Clients Worldwide</small>
               </article>
           </div>
 
@@ -41,36 +41,42 @@ const about = () => {
               <article className="about__card">
                 <VscFolderLibrary className="about__icon"/>
                 <h5>Projects </h5>
-                <small>80+ Projects Completed</small>
+                <small>6 Projects Completed</small>
               </article>
             </div>
           {/* BOX 4 */}
-            <div className="about__content">
+            {/* <div className="about__content">
               <article className="about__card">
                 <FiUsers className="about__icon"/>
                 <h5>Example</h5>
                 <small>Can Be Easily Removed!</small>
               </article>
-          </div>
+          </div> */}
           {/* BOX 5 */}
-          <div className="about__content">
+          {/* <div className="about__content">
               <article className="about__card">
                 <FiUsers className="about__icon"/>
                 <h5>Example</h5>
                 <small>Can Be Easily Removed!</small>
               </article>
-          </div>
+          </div> */}
           {/* BOX 6 */}
-          <div className="about__content">
+          {/* <div className="about__content">
               <article className="about__card">
                 <FiUsers className="about__icon"/>
                 <h5>Example</h5>
                 <small>Can Be Easily Removed!</small>
               </article>
-          </div>
+          </div> */}
         </div>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima illo praesentium consequatur ad facere, adipisci placeat. Voluptatum, esse magnam beatae vero ut natus ipsum voluptatem, consequatur ad eveniet fugiat aliquam.
+          Ever since I was a young kid I have always had an interest in computers. Videogames piqued my interest like most boys my age but it wasn't till I found out about programming is where that fun really started.
+          </p>
+          <p>
+          The passion of being able to create and innovate anything drove me toward computer science as a kid. This same passion brought me back to the life of a software engineer as college student.
+          </p>
+          <p>
+          In my spare time, I run my own production company that specializes in photography and videography work as well as making music. I have been taking pictures for about 9 years now and I really enjoy being able to tell others stories through artistic expression. Becasue of this I can use the adobe suite with ease. I also have an extensive knowledge in marketing and graphic design. While I do have a plethora of skills I understand that there is always something more to learn. That is why I have committed myself to a life of learning.
           </p>
           <a href="#contact" className="btn btn-primary btn-aboutme">Let's Talk</a>
         </div>
